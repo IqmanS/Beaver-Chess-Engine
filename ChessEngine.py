065065c9f55551b7d754ca8c59e2b2af14932368
Iqman Singh Bhatia <ibhatia_be21@thapar.edu>
2023-04-15 12:20:22 +0530

    Make Move

--- ChessEngine.py
+++ ChessEngine.py
@@ -1,8 +1,9 @@
+import numpy as np
 
 class GameState():
     def __init__(self):
         # 8X8 2D MATRIX
-        self.board = [
+        self.board = np.array([
             ["bR", "bN", "bB", "bK", "bQ", "bB", "bN", "bR"],
             ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
             ["--", "--", "--", "--", "--", "--", "--", "--"], #empty
@@ -11,7 +12,36 @@ class GameState():
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
             ["wR", "wN", "wB", "wK", "wQ", "wB", "wN", "wR"],
-        ]
+        ])
         self.whiteToMove = True
         self.moveLog = []
         
+    def makeMove(self,gameMove):
+        self.board[gameMove.startRow][gameMove.startCol] = "--"
+        self.board[gameMove.endRow][gameMove.endCol] = gameMove.pieceMoved
+        self.moveLog.append(gameMove.toChessNotation())
+        
+        #swap player turns
+        self.whiteToMove = not self.whiteToMove
+        
+        
+class GameMove():
+    
+    ranksToRows = {"1":7,"2":6,"3":5,"4":4,"5":3,"6":2,"7":1,"8":0}
+    rowsToRank = {v:k for k,v in ranksToRows.items()}
+    filesToCol = {"a":7,"b":6,"c":5,"d":4,"e":3,"f":2,"g":1,"h":0}
+    colToFiles = {v: k for k, v in filesToCol.items()}
+    
+    def __init__(self,board,startSquare,endSquare):
+        self.startRow = startSquare[0]
+        self.startCol = startSquare[1]
+        self.endRow = endSquare[0]
+        self.endCol = endSquare[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.placeCaptured = board[self.endRow][self.endCol]
+        
+    def toChessNotation(self):
+        rankFileStart = self.colToFiles[self.startCol] + self.rowsToRank[self.startRow]
+        rankFileEnd = self.colToFiles[self.endCol] + self.rowsToRank[self.endRow]
+        return str(rankFileStart+rankFileEnd)
+    
--- ChessMain.py
+++ ChessMain.py
@@ -2,8 +2,12 @@
 # Handles User Input and display current game state
 
 import pygame as p
+import numpy as np
 from ChessEngine import *
+
 p.init()
+p.display.set_caption('Beaver')
+FONT = p.font.Font('freesansbold.ttf', 10)
 WIDTH = HEIGHT = 512
 DIMENSION = 8
 CELL_SIZE = HEIGHT// DIMENSION
@@ -12,12 +16,12 @@ IMAGES = {}
 GREY = (119, 132, 102)
 WHITE =  (189, 196, 180)
 BG = (60, 70, 50)
-INDENT = 12
+INDENT = 18
 
 #should be called only once
 def loadImages():
-    pieces = ["bR", "bN", "bB", "bK", "bQ", "bB", "bB", "bN", "bR","bP",
-              "wR", "wN", "wB", "wK", "wQ", "wB", "wB", "wN", "wR","wP"]
+    pieces = np.array(["bR", "bN", "bB", "bK", "bQ", "bB", "bB", "bN", "bR","bP",
+              "wR", "wN", "wB", "wK", "wQ", "wB", "wB", "wN", "wR","wP"])
     for piece in pieces:
         IMAGES[piece] = p.transform.scale(p.image.load("images/"+piece+".png"),(CELL_SIZE,CELL_SIZE))
         #now we can access the IMAGES dictionary
@@ -25,6 +29,18 @@ def loadImages():
 # GUI
 def drawGameState(screen,gameState):
     drawBoardAndPieces(screen,gameState)       #draw cells & pieces
+
+def drawRanksAndFiles(screen):
+    for i in range(DIMENSION):
+        rank = str(DIMENSION-i)
+        rankBox = FONT.render(rank, True, WHITE)
+        screen.blit(rankBox, (INDENT//2, i*CELL_SIZE + INDENT +CELL_SIZE//2))
+    files = {7: "a",6:  "b",5: "c",4: "d",3: "e",2: "f",1: "g",0: "h"}
+    for j in range(DIMENSION):
+        file = files[DIMENSION-j-1]
+        fileBox = FONT.render(file, True, WHITE)
+        screen.blit(fileBox, (j*CELL_SIZE + INDENT +CELL_SIZE//2,HEIGHT+ INDENT+INDENT//4))
+
 
 def drawBoardAndPieces(screen,gameState):
     for i in range(DIMENSION):
@@ -35,7 +51,7 @@ def drawBoardAndPieces(screen,gameState):
             square =p.Rect(x,y,CELL_SIZE,CELL_SIZE)
             p.draw.rect(screen,color,square,width=CELL_SIZE)
             piece = gameState.board[i][j]
-            if piece!="--":
+            if piece != "--":
                 x = j * CELL_SIZE + INDENT
                 y = i * CELL_SIZE + INDENT
                 screen.blit(IMAGES[piece],(x,y))
@@ -45,15 +61,39 @@ def main():
     clock = p.time.Clock()
     screen.fill(BG)
     gameState = GameState()
-    print(gameState.board)
+    # print(gameState.board)
     loadImages() # ONLY ONCEEEEEEE
-    drawGameState(screen, gameState)
+    drawRanksAndFiles(screen)
     running = True
-    
+    lastSqSelected = () #(row,col)
+    playerClicks = [] #(6,4) -> (4,4)
     while running:
+        
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            if e.type == p.MOUSEBUTTONDOWN:
+                loc = p.mouse.get_pos()
+                
+                col = (loc[0] - INDENT ) // CELL_SIZE
+                row = (loc[1] - INDENT ) // CELL_SIZE
+                if col>=0 and col < DIMENSION and row >=0 and row < DIMENSION:
+                    if (row,col) == lastSqSelected:
+                        lastSqSelected = ()
+                        playerClicks = []
+                    else:
+                        lastSqSelected = (row,col)
+                        playerClicks.append(lastSqSelected)
+                    if len(playerClicks)==2:
+                        # print(playerClicks)
+                        move = GameMove(board=gameState.board,
+                                        startSquare=playerClicks[0],
+                                        endSquare=playerClicks[1])
+                        gameState.makeMove(move)
+                        print(move.toChessNotation())
+                        lastSqSelected = ()
+                        playerClicks = []
+        drawGameState(screen, gameState)
         clock.tick(MAX_FPS)
         p.display.flip()
         
